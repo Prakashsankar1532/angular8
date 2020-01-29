3755eced2f4e1883396358af8d0ae45f0f47cb0d
Prakashsankar1532 <prakashsankar1532@gmail.com>
2020-01-29 18:24:37 +0530

    cmd upload

---
+++ task/test.js
@@ -0,0 +1,7 @@
+// $.getJSON( "test.json", function( json ) {
+//     console.log( "JSON Data received, name is " + json.name);
+// });
+
+$.getJSON("test.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
